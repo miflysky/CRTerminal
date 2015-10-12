fe165fbaabed7c82fb2b19cc04618e012e0f070f
tianxiang <tianxiang.mi@tieto.com>
2015-10-12 18:08:53 +0800

    wifi ready

---
+++ src/com/tieto/crterminal/CRTClient.java
@@ -0,0 +1,45 @@
+package com.tieto.crterminal;
+
+public class CRTClient {
+
+	public CRTClient() {
+		// TODO: initialize...
+	}
+
+	public void startSocketClient() {
+		// TODO: create thread
+	}
+
+	public void stopSocketClient() {
+		// TODO: stop thread
+	}
+
+	public boolean ClientConnect() {
+
+		return true;
+	}
+
+	public boolean ClientDisconnect() {
+
+		return true;
+	}
+
+	public boolean ClientLogin() {
+
+		return true;
+	}
+
+	public boolean ClientLogout() {
+
+		return true;
+	}
+
+	public class ClientThread extends Thread {
+
+		public ClientThread(String threadName) {
+			super(threadName);
+		}
+
+	}
+
+}
